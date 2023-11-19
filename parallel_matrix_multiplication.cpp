30385d38e7998012dcabcdae335883c7a56dd26e
potassiummmm <1574996073@qq.com>
2023-11-19 14:27:20 +0800

    assignment 1 finished

--- parallel_matrix_multiplication.cpp
+++ parallel_matrix_multiplication.cpp
@@ -1,13 +1,3 @@
-# 作业一: 并行矩阵乘法
-## 问题陈述
-编写⼀个基于oneAPI的C++/SYCL程序来执行矩阵乘法操作。
-## 项目简介
-本项目基于Intel提供的oneAPI工具集，编写C++/SYCL程序利用基于SYCL的编程模型在GPU上实现矩阵乘法的计算。
-## 技术框架
-### Intel oneAPI
-
-## 具体实现
-```cpp
 #include <sycl/sycl.hpp>
 #include <random>
 #include <chrono>
@@ -53,7 +43,7 @@ public:
             auto resultAcc = resultBuffer.get_access<sycl::access::mode::write>(cgh);
             auto matrixAAcc = matrixABuffer.get_access<sycl::access::mode::read>(cgh);
             auto matrixBAcc = matrixBBuffer.get_access<sycl::access::mode::read>(cgh);
-            // 分块
+
             cgh.parallel_for<class BlockMatrixMultiplyKernel>(
                 sycl::nd_range<2>(sycl::range<2>(MATRIX_SIZE, MATRIX_SIZE), sycl::range<2>(BLOCK_SIZE, BLOCK_SIZE)),
                 [=](sycl::nd_item<2> item) {
@@ -137,42 +127,3 @@ int main() {
 
     return 0;
 }
-```
-
-## 运行结果
-随机生成1024*1024的两个矩阵，进行乘法运算，并将并行运算的结果与常规串行运算结果相比较，输出如下：
-
-```
-Job has been submitted to Intel(R) DevCloud and will execute soon.
-
-Job ID                    Name             User            Time Use S Queue
-------------------------- ---------------- --------------- -------- - -----
-2432120.v-qsvr-1           ...ub-singleuser u207452         00:00:41 R jupyterhub     
-2432161.v-qsvr-1           ...x_multiply.sh u207452                0 Q batch          
-
-Waiting for Output ████████████████████ Done⬇
-
-########################################################################
-#      Date:           Sat 18 Nov 2023 10:13:11 PM PST
-#    Job ID:           2432161.v-qsvr-1.aidevcloud
-#      User:           u207452
-# Resources:           cput=75:00:00,neednodes=1:gpu:ppn=2,nodes=1:gpu:ppn=2,walltime=06:00:00
-########################################################################
-
-## u207452 is compiling SYCL_Essentials Module2 -- SYCL Program Structure sample - 7 of 7 matrix_multiply.cpp
-Time taken for scalar matrix multiplication:: 1.77183 seconds
-Time taken for parallel matrix multiplication:: 0.148906 seconds
-Matrix multiplication successfully run on the device
-
-########################################################################
-# End of output for job 2432161.v-qsvr-1.aidevcloud
-# Date: Sat 18 Nov 2023 10:13:23 PM PST
-########################################################################
-
-Job Completed in 20 seconds.
-```
-
-可以看到，并行运算与串行运算结果一致，且具有很高的执行效率。
-
-## 学到了什么
-通过本次实验，我初次接触了高性能计算以及Intel提供的高性能计算平台，利用oneAPI这一开发工具套件，上手实操完成了对矩阵乘法的并行优化。此外，我切实感受了SYCL编程模型及相应库函数的强大之处，对我未来的工作大有裨益。
